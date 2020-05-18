c8b257911f4f1188a65b7a39e8475e483bbf0b51
Matthew Wilkins, PhD <mrwilkins06@gmail.com>
2020-05-18 18:51:18 -0500

    Added button icon to make report, busy icon if render takes a while, and fixed killer error if you push button without uploading at step 1

--- app.R
+++ app.R
@@ -14,18 +14,30 @@ keyURLs<-read.csv("www/keyURLs.csv") #ignore error
 keyURLs$gid<-sapply(keyURLs$URL,getSheetID)
 keyURLs$dlURL<-sapply(keyURLs$gid,function(x){paste0("https://docs.google.com/spreadsheets/d/",x,"/export?format=csv")})
 
-library(shiny);require(shinydashboard);library(rmarkdown);require(kableExtra)
+library(shiny);require(shinydashboard);library(rmarkdown);require(kableExtra);require(shinybusy)
 source("gradingScript.R")
 
 ui <- navbarPage(title = "GoogleFormGrader",
+  
   tabPanel("Setup",
+    #Custom styling
+    tags$head(tags$style(HTML({"
+    #box{   width:300px;padding:10px 10px 0px 10px; 
+            margin:5px;vertical-align:middle;text-align:center; 
+    }
+    .bad{background-color: #960061}
+    .good{background-color: #3DFF90}
+    .info{border: 3px solid #3e0055;background-color:#f0f4ff;}
+      "}
+    ))),
+   
     # Boxes need to be put in a row (or column)
       div(img(src="GPlogo.png",width=300),style="padding:10px"),
-      div(style="background-color:#f0f4ff;width:300px;padding:10px 10px 0px 10px; margin:5px; border: 3px solid #3e0055",
-        p("This app is designed to help teachers grade and summarize student performance on Galactic Polymath-styled webquests", a("like this one",href="https://docs.google.com/document/d/1CJN5xuUUsLMO481qcUBDshBzkmde0JGhQeEBGsysb8Y/edit?usp=sharing",target="_blank"),".",style="font-weight:500;color:#3e0055;")
+      div(id="box",class="info",
+        p("This app is designed to help teachers grade and summarize student performance on Galactic Polymath-styled webquests", a("like this one",href="https://docs.google.com/forms/d/e/1FAIpQLSeWG1D8MuoVRNg5CQiTNetu17-ros5yfMIRdWqCQAPRW1GZQQ/viewform?usp=sf_link",target="_blank"),".",style="font-weight:500;color:#3e0055;")
         ),
       h3("Step 1:"),
-      p("Upload student form responses, downloaded as \"Comma-Separated Values\" format from Google Sheets. Here's an",a("example video tutorial-- but choose CSV, not Excel format.",href="https://www.youtube.com/watch?v=iySTMUYFY9k")),
+      p("Upload student form responses, downloaded as \"Comma-Separated Values\" format from Google Sheets. Here's an",a("example video tutorial-- but choose CSV, not Excel format.",href="https://www.youtube.com/watch?v=iySTMUYFY9k",target="_blank")),
       fluidRow(style="padding-left:5em",fileInput("responses", "Choose .CSV File",
                 multiple = FALSE,
                 accept = c("text/csv",
@@ -41,10 +53,12 @@ ui <- navbarPage(title = "GoogleFormGrader",
                            "text/comma-separated-values,text/plain",
                            ".csv"))
             ),
-    h3("Step 3:"),
-    verbatimTextOutput("console"),
-     h3("Step 4:"),
-    actionButton('make',"Make Report")
+    # h3("Step 3:"),
+    # verbatimTextOutput("console"),
+     h3("Step 3:"),
+    actionButton('make',tags$table(tags$tr(tags$td(img(src='gpicon.ico',style="max-height:30px")),tags$td(p(strong("Make Report"),style="font-size: 100%;"))))),
+    
+  uiOutput("dlprog")
   ),#End Setup Panel
   
   tabPanel("Preview",
@@ -65,6 +79,15 @@ ui <- navbarPage(title = "GoogleFormGrader",
   
 )
 
+
+
+
+
+
+
+
+
+
 server <- function(input, output) {
 ################## 
 ### Setup Tab
@@ -77,34 +100,52 @@ server <- function(input, output) {
     })
   
 observe(
-  if(length(input$custKey)==0){
+  if(length(input$custKey)==0){ #if no custom key, download & import selected default key
     req(vals$studentResponses)
     keyfile<-file.path(tempdir(),"keyfile.csv")
-    download.file(keyURLs$dlURL[match(input$whichKey,keyURLs$keyID)],keyfile)
+    keyRow<-match(input$whichKey,keyURLs$keyID)
+    download.file(keyURLs$dlURL[keyRow],keyfile)
     vals$key<-read.csv(keyfile,stringsAsFactors =F)
-    }else{read.csv(vals$key<-input$custKey$datapath,stringsAsFactors =F)}
+    vals$lessonName<-keyURLs$lessonName[keyRow]
+  }else{vals$key <- read.csv(input$custKey$datapath,stringsAsFactors =F) #otherwise use supplied key
+      vals$lessonName<-basename(input$custKey$datapath)}
  )
   
 output$console<-renderText( head(as.matrix(vals$studentResponses)))
 
 #Make Preview Report when button pushed
 observeEvent(input$make,{
+  req(vals$key,vals$studentResponses)
   vals$gradesheet<-grade(vals$key,vals$studentResponses) 
-  
+  vals$prog<-"rendering"
   f<-"preview.html"
   # Render the document
-  params=list(gradesheet=vals$gradesheet)
-  rmarkdown::render(
-    'template.Rmd',
-    output_file = f,
-    envir = new.env(parent = globalenv()),
-    params=params
+  params=list(lessonName=vals$lessonName,gradesheet=vals$gradesheet)
+  try({
+      rmarkdown::render(
+        'template.Rmd',
+        output_file = f,
+        envir = new.env(parent = globalenv()),
+        params=params
+      )}
   )
-  
-})
+  vals$prog<-"done"
+  })
 
+output$dlprog<-renderUI(
+  if(is.null(vals$prog)){
+    div(style="margin: 10px; padding:10px 0px 10px 0px;")
+  }else{
+    if(vals$prog=="rendering"){
+      tags$div(
+          style = "display: table-cell; width: 150px; height: 100px; margin: 10px;padding:10px 0px 10px 0px;",
+          spin_epic("semipolar",color="#3e0055"))
+  }else{div(id="box",class="good",p("Grades and report available in preview tab"))}}
   
-  
+)
+
+
+
 ##################  
 ### Preview tab
 prevFileTest<-reactiveFileReader(1000,NULL,"preview.html",file.exists) 
